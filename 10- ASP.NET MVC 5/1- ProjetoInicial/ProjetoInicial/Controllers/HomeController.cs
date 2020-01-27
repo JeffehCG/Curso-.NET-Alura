57457135b2bfda8f872542be76b5c899e37e44e5
Jefferson Costa Gomes <31323813+JeffehCG@users.noreply.github.com>
2020-01-26 22:38:43 -0300

    MVC 5

---
+++ 10- ASP.NET MVC 5/1- ProjetoInicial/ProjetoInicial/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+// Controllers iram cuidar e tratar requisições web da API
+// Para criar uma controller clique com o botão direito na pasta controllers/Add/Controller
+namespace ProjetoInicial.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // Para criar uma view para o metodo abaixo clique com o botão direito em cima de Index() , Add View
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 10- ASP.NET MVC 5/2- CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,61 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            // Listando categorias para o dropdown do formulario
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        //public ActionResult Adiciona(string nome, float preco, string descricao, int quantidade, int categoriaId)
+        //{
+        //    Produto produto = new Produto()
+        //    {
+        //        Nome = nome,
+        //        Preco = preco,
+        //        Descricao = descricao,
+        //        Quantidade = quantidade,
+        //        CategoriaId = categoriaId
+        //    };
+
+        //    ProdutosDAO dao = new ProdutosDAO();
+        //    dao.Adiciona(produto);
+        //    return View();
+        //}
+
+        //Ou
+
+
+        // Esse metodo só ira receber requisições do tipo POST
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+
+            // Redirecionando para o metodo Index, Controller Produto
+            return RedirectToAction("Index", "Produto");
+        }
+    }
+}
