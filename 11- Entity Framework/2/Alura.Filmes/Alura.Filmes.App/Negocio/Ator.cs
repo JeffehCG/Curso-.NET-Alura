299e2b29483d2348de346486f364ba17e03b3a60
Jefferson <dez.jefferson2@gmail.com>
2020-06-08 21:16:26 -0300

    Conhecimento em Entity Framework Core

---
+++ 11- Entity Framework/2/Alura.Filmes/Alura.Filmes.App/Negocio/Ator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Negocio
+{
+    [Table("actor")] // Anotação do Entity (Define que a classe 'Ator' mapeaia a tabela 'actor' do banco)
+    public class Ator
+    {
+        [Column("actor_id")] // Anotação do Entity (Define que a propriedade Id mapeia a colluna 'actor_id' do banco)
+        public int Id { get; set; }
+
+        [Column("first_name")]
+        public string PrimeiroNome { get; set; }
+
+        [Column("last_name")]
+        public string UltimoNome { get; set; }
+
+        public override string ToString()
+        {
+            return $"Ator ({Id}) {PrimeiroNome} {UltimoNome}";
+        }
+    }
+}
---
+++ 11- Entity Framework/2/Alura.Filmes/Alura.Filmes.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using System;
+using System.Linq;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var context = new AluraFilmesContext())
+            {
+                context.LogSQLToConsole();
+
+                var atores = context.Atores.ToList();
+                foreach (var item in atores)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
